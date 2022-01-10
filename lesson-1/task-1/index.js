33555f2b96dc3681bfaabd080a8c8e60015e32c3
Denys <denzzlle171@gmail.com>
2022-01-10 16:25:50 +0300

    save change 0001

--- lesson-1/task-1/index.js
+++ lesson-1/task-1/index.js
@@ -1,11 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const rootElement = document.querySelector('#root');
-//
-// const greetingElem = document.createElement('div');
-// greetingElem.classList.add('greeting');
-// greetingElem.textContent = 'Hello, React!';
-
-// rootElement.append(greetingElem);
-
 const greetingElem = React.createElement(
   'div',
   { className: 'greeting' },
--- lesson-1/task-2/index.js
+++ lesson-1/task-2/index.js
@@ -1,23 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const rootElement = document.querySelector('#root');
-
-// const renderGreeting = (elem) => {
-//   const containerElem = document.createElement('div');
-//   containerElem.classList.add('greeting');
-
-//   const titleElem = document.createElement('h1');
-//   titleElem.classList.add('greeting__title');
-//   titleElem.textContent = 'Hello, world!';
-
-//   const textElem = document.createElement('p');
-//   textElem.classList.add('greeting__text');
-//   textElem.textContent = "I'm learning React";
-
-//   containerElem.append(titleElem, textElem);
-
-//   elem.append(containerElem);
-// };
-
-// renderGreeting(rootElement);
 
 const element = React.createElement('div', { className: 'greeting' }, [
   React.createElement('div', { className: 'greeting__title' }, 'Hello, world!'),
