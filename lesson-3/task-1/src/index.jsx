5b24403d6847fa20c8df15e3a2d2ce46213a52cd
Denys <denzzlle171@gmail.com>
2022-01-13 00:12:21 +0300

    test 3

--- lesson-3/task-1/src/index.jsx
+++ lesson-3/task-1/src/index.jsx
@@ -5,7 +5,7 @@ import './search.scss';
 
 const rootElement = document.querySelector('#root');
 
-export const Search = (props) => {
+const Search = (props) => {
   return (
     <div className="search">
       <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
@@ -17,5 +17,5 @@ export const Search = (props) => {
   );
 };
 
-ReactDOM.render(<Search name="Tom" />, rootElement);
-// export default Search;
+// ReactDOM.render(<Search name="Tom" />, rootElement);
+export default Search;
