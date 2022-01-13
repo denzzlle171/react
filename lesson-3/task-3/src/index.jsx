4438bdb75a6919f152d6c2b16f14e4da5ab29103
Denys <denzzlle171@gmail.com>
2022-01-14 02:01:33 +0300

    save test 8

--- lesson-3/task-3/src/index.jsx
+++ lesson-3/task-3/src/index.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Comment from './Comment';
+import App from './App.jsx';
 
 const rootElement = document.querySelector('#root');
 
-const author = {
-  name: 'Tom',
-  аvatarUrl: 'https://avatars1.githubusercontent.com',
-};
-
-ReactDOM.render(
-  <Comment
-    user={author}
-    text="Good job!"
-    date={new Date('2019-01-01T11:32:19.566Z')}
-  />,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
