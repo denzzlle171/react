7950c3e0dea8d011172ba900f7f5cddbc17e826c
Denys <denzzlle171@gmail.com>
2022-01-14 18:53:18 +0300

    test 22

--- lesson-3/task-3/src/Comment.jsx
+++ lesson-3/task-3/src/Comment.jsx
@@ -6,7 +6,6 @@ import UserInfo from './UserInfo';
 const formatDate = (date) => moment(date).format('DD MMM YYYY');
 
 function Comment(props) {
-  console.log(props)
   return (
     <div className="comment">
       <UserInfo user={props.author} />
--- lesson-3/task-3/src/UserInfo.jsx
+++ lesson-3/task-3/src/UserInfo.jsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import Avatar from './Avatar';
 
-// const UserInfo = (props) => {
-//   console.log(props)
-//   return (
-//     <div className="user-info">
-//       <Avatar avatarUrl={props.user.аvatarUrl} name={props.user.name} />
-//       <div className="user-info__name">{props.user.name}</div>
-//     </div>
-//   );
-// };
-
 function UserInfo(props) {
-    console.log(props)
     return (
       <div className="user-info">
         <Avatar avatarUrl={props.user.аvatarUrl} name={props.user.name} />
