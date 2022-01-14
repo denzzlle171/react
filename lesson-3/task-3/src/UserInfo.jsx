f18829463e4f31d8ab6c65c930e032b04019ad06
Denys <denzzlle171@gmail.com>
2022-01-14 18:07:15 +0300

    test 4

--- lesson-3/task-3/src/UserInfo.jsx
+++ lesson-3/task-3/src/UserInfo.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import Avatar from './Avatar';
 
-const UserInfo = (props) => {
-  console.log(props)
-  return (
-    <div className="user-info">
-      <Avatar avatarUrl={props.user.аvatarUrl} name={props.user.name} />
-      <div className="user-info__name">{props.user.name}</div>
-    </div>
-  );
-};
+// const UserInfo = (props) => {
+//   console.log(props)
+//   return (
+//     <div className="user-info">
+//       <Avatar avatarUrl={props.user.аvatarUrl} name={props.user.name} />
+//       <div className="user-info__name">{props.user.name}</div>
+//     </div>
+//   );
+// };
+
+function UserInfo (props) {
+    console.log(props)
+    return (
+      <div className="user-info">
+        <Avatar avatarUrl={props.user.аvatarUrl} name={props.user.name} />
+        <div className="user-info__name">{props.user.name}</div>
+      </div>
+    );
+  };
+
 
 export default UserInfo;
