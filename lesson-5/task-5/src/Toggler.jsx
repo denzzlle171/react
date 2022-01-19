3dc8d1583c1f8a9d7b0967db687b811d9d8d7c57
Denys <denzzlle171@gmail.com>
2022-01-20 00:53:51 +0300

    test

--- lesson-5/task-5/src/Toggler.jsx
+++ lesson-5/task-5/src/Toggler.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 
 class Toggler extends React.Component {
-  buttonСondition = () => {
-    const elButton = document.querySelector('.toggler');
-
-    if (elButton.textContent === 'Off') {
-      elButton.textContent = 'On';
+  buttonСondition = (e) => {
+    if (e.target.textContent === 'Off') {
+      e.target.textContent = 'On';
     } else {
-      elButton.textContent = 'Off';
+      e.target.textContent = 'Off';
     }
   };
 
   render() {
     return (
-      <button onClick={() => this.buttonСondition()} className="toggler">
+      <button onClick={this.buttonСondition} className="toggler">
         Off
       </button>
     );
   }
 }
 export default Toggler;
+
+{
+  // const elButton = document.querySelector('.toggler');
+  /* <button onClick={() => this.buttonСondition()} className="toggler"></button> */
+}
