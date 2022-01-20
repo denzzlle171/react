fd0efcbd26de9aad292af44351e4d477cd5efd27
Denys <denzzlle171@gmail.com>
2022-01-21 00:45:29 +0300

    save test 3

--- lesson-5/task-6/src/ColorPicker.jsx
+++ lesson-5/task-6/src/ColorPicker.jsx
@@ -1,41 +1,45 @@
 import React from 'react';
 
 class ColorPicker extends React.Component {
+
+
   constructor(props) {
     super(props);
+
     this.state = {
       activeColor: null,
     };
   }
 
-  setTitle = title => {
+  setTitle = (title) => {
     this.setState({
       activeColor: title,
+    
     });
-  };
+  }; 
 
   clearTitle = () => this.setTitle(null);
 
   render() {
     return (
       <div>
-        <div className="picker__title"></div>
+        <div className="picker__title">{this.state.activeColor}</div>
         <div>
           <button
             onMouseEnter={() => this.setTitle('Coral')}
             onMouseLeave={this.clearTitle}
             className="picker__button picker__button_coral"
           ></button>
-          
+
           <button
-            onMouseOver={this.showText.bind(this, 'Aqua')}
-            onMouseOut={this.cleanText}
+            onMouseEnter={() => this.setTitle('Aqua')}
+            onMouseLeave={this.clearTitle}
             className="picker__button picker__button_aqua"
           ></button>
-          
+
           <button
-            onMouseOver={this.showText.bind(this, 'Bisque')}
-            onMouseOut={this.cleanText}
+            onMouseEnter={() => this.setTitle('Bisque')}
+            onMouseLeave={this.clearTitle}
             className="picker__button picker__button_bisque"
           ></button>
         </div>
@@ -43,6 +47,5 @@ class ColorPicker extends React.Component {
     );
   }
 }
-export default ColorPicker;
 
-// Red;
+export default ColorPicker;
