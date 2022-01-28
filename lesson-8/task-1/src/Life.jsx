4eaf2c2132354635beb0fcb00773c8c7dc062d56
Denys <denzzlle171@gmail.com>
2022-01-29 00:46:18 +0300

    test 1

--- lesson-8/task-1/src/Life.jsx
+++ lesson-8/task-1/src/Life.jsx
@@ -20,7 +20,7 @@ class Life extends Component {
     return nextProps.number % 2;
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate() {
     console.log(
       'componentDidUpdate(prevProps, prevState): some updates based on new props'
     );
