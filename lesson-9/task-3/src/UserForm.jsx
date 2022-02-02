b53202dc25ecb777cc2552833363aa05d0a6f294
Denys <denzzlle171@gmail.com>
2022-02-02 19:02:14 +0300

    save 111

--- lesson-9/task-3/src/UserForm.jsx
+++ lesson-9/task-3/src/UserForm.jsx
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 
 class UserForm extends Component {
-
-
-  onSubmit = (event) => {
+  handleSubmit = (event) => {
     event.preventDefault();
-    const formData = [...new FormData(this.formRef)]
-      .reduce((acc, [name, value]) => ({ ...acc, [name]: value }), {})
-    this.props.createUser(formData);
+    const formData = [...new FormData(this.formRef)].reduce(
+      (acc, [name, value]) => ({ ...acc, [name]: value }),
+      {}
+    );
+    this.props.onSubmit(formData);
   };
 
-  setRef = node => {
+  setRef = (node) => {
     this.formRef = node;
-  }
+  };
 
   render() {
     return (
-      <form ref={this.setRef} className="login-form" onSubmit={this.onSubmit}>
+      <form
+        ref={this.setRef}
+        className="login-form"
+        onSubmit={this.handleSubmit}
+      >
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">
