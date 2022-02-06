d9470874cd75b68d86404551dc7fa832fcef2276
Denys <denzzlle171@gmail.com>
2022-02-06 03:25:13 +0300

    test 1

---
+++ lesson-9/task-4/src/UsersList.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import User from './User.jsx';
+import Filter from './Filter.jsx';
+
+class UsersList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+      usersArr: this.props.users,
+    };
+  }
+  //
+  handleChange = (event) => {
+this.setState  ({
+      value: event.target.value,
+})
+    
+const filtredArr = this.props.users.filter(
+  (user) => user.name.toLowerCase() === event.target.value.toLowerCase()
+);
+    
+   const changeArr = !(this.state.value === '') ? filtredArr : this.props.users;
+    
+    console.log(changeArr);
+    this.setState({
+      usersArr: changeArr,
+    });
+   
+  };
+  //
+  render() {
+    return (
+      <div>
+        <Filter
+          filterText={this.state.value}
+          count={this.state.usersArr.length}
+          onChange={this.handleChange}
+        />
+        <ul className="users">
+          {this.state.usersArr.map((user) => (
+            <User key={user.id} name={user.name} age={user.age} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+export default UsersList;
