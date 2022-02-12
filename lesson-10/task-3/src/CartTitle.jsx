9f8ed1c48615e549cb52b21e4676a43bca02b56f
Denys <denzzlle171@gmail.com>
2022-02-12 16:51:15 +0300

    test 0

--- lesson-10/task-3/src/CartTitle.jsx
+++ lesson-10/task-3/src/CartTitle.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const CartTitle = ({ userName, count }) => {
   return (
-    <div className="cart-title">
-      {`${userName}, you added ${count} product`}
-    </div>
+    <div className="cart-title">{`${userName}, you added ${count} items`}</div>
   );
 };
 export default CartTitle;
