bfefb6db3eead26d3e29a886e5e9ae8c8253ef83
Denys <denzzlle171@gmail.com>
2022-02-14 02:27:33 +0300

    lesson 10 task (4)

---
+++ lesson-10/task-5/src/SearchBar.jsx
@@ -0,0 +1,47 @@
+import React  from 'react';
+
+
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+  handleFilterTextChange=(e)=> {
+    this.props.onFilterTextChange(e.target.value);
+  }
+
+  handleInStockChange=(e)=> {
+    this.props.onInStockChange(e.target.checked);
+  }
+
+  render() {
+   
+    console.log(this.props);
+    
+
+    const {filterText, inStockOnly} = this.props
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={filterText}
+          onChange={this.handleFilterTextChange}
+        />
+        <p>
+          <input
+            type="checkbox"
+            checked={inStockOnly}
+            onChange={this.handleInStockChange}
+          />
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
+  
