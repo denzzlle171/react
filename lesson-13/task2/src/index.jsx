f2ae3e10f7d6b6771ccff0ddaac967d4620eaa39
Denys <denzzlle171@gmail.com>
2022-02-21 20:45:27 +0300

    lesson-13 task 2

--- lesson-13/task2/src/index.jsx
+++ lesson-13/task2/src/index.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App.jsx';
 
-
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(
-    <App />
-    , rootElement)
+ReactDOM.render(<App  />, rootElement);
