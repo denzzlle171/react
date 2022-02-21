6fb6d922aafc332f1dbcc85c70ef9f72cf71dc9e
Denys <denzzlle171@gmail.com>
2022-02-21 20:50:05 +0300

    push test1

--- lesson-13/task2/src/Product.jsx
+++ lesson-13/task2/src/Product.jsx
@@ -4,6 +4,10 @@ import { useParams } from 'react-router-dom';
 const Product = () => {
   const { productId } = useParams();
 
-  return <div className="product">{`Product is a ${productId}`}</div>;
+  return <div className="product">{productId}</div>;
 };
 export default Product;
+
+// {
+//   `Product is a ${productId}`;
+// }
