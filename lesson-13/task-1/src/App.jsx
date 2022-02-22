928e2e963c15c8df20d879ae900e846e10058ea8
Denys <denzzlle171@gmail.com>
2022-02-22 22:41:39 +0300

    lesson 14 task 1

--- lesson-13/task-1/src/App.jsx
+++ lesson-13/task-1/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Contacts from './Contacts'
 import Products from './Products';
 import Home from './Home';
 
 const App = () => {
     return (
-      <div className="page">
-        <BrowserRouter>
-          <Route to="/"><Home /></Route>
-          <Route to="/products"><Products /></Route>
-          <Route to="/contacts"><Contacts /></Route>
-        </BrowserRouter>
+      <div className="page" >
+        <Routes>
+          <Route to="/" component={Home}/>
+          <Route to="/products" component={Products}/>
+          <Route to="/contacts" component={Contacts}/>
+        </Routes>
       </div>
     );
 }
--- lesson-13/task-1/src/index.jsx
+++ lesson-13/task-1/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App.jsx';
 
@@ -7,5 +8,8 @@ import App from './App.jsx';
 const rootElement = document.querySelector('#root');
 
 ReactDOM.render(
+  <BrowserRouter>
     <App />
-    , rootElement)
+  </BrowserRouter>,
+  rootElement
+);
